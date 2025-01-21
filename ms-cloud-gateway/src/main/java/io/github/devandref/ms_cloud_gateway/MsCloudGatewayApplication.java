89d99ab5d677094a98d35ca122acb23beaf0ff1e
devandref <andredepaulacontato@gmail.com>
2025-01-21 15:21:55 -0300

    include: impl do gateway centralizador de comunicação entre as apis

--- ms-cloud-gateway/src/main/java/io/github/devandref/ms_cloud_gateway/MsCloudGatewayApplication.java
+++ ms-cloud-gateway/src/main/java/io/github/devandref/ms_cloud_gateway/MsCloudGatewayApplication.java
@@ -2,12 +2,27 @@ package io.github.devandref.ms_cloud_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsCloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsCloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+					.route(r -> r.path("/clientes/**").uri("lb://ms-clientes"))
+				.build();
 	}
 
 }
