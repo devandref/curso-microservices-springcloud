99344a593d58c26f72c40a6e0ad90a8d16d2d3ac
devandref <andredepaulacontato@gmail.com>
2025-01-23 11:42:52 -0300

    include: init do microservico de cartoes

---
+++ ms-cartoes/src/test/java/io/github/devandref/ms_cartoes/MsCartoesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devandref.ms_cartoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCartoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
